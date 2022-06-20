5a241cf0a3ecd33b3a65853e243624630e840d33
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-20 20:05:46 +0200

    Pass the form to the template.

--- generator/views.py
+++ generator/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .forms import IdeaForm
+
 
 def index(request):
-    context = {'phrase_to_render': "popo"}
+    form = IdeaForm()
+    context = {'form': form}
     return render(request, "generator/index.html", context)
