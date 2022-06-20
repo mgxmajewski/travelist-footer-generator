a03b5696c3ad697e45bb5e4af72d6670827d4615
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-20 20:56:42 +0200

    Adjust form style.

--- generator/forms.py
+++ generator/forms.py
@@ -4,8 +4,7 @@ from django import forms
 class IdeaForm(forms.Form):
     your_name = forms.CharField(label='Mamy taki pomysł, żeby...',
                                 max_length=100,
-                                widget=forms.TextInput(
-                                    attrs={'class': 'myfieldclass',
-                                           'placeholder': 'Poinformuj swoich partnerów o wymarzonym pomyśle wakacyjnym. '
+                                widget=forms.Textarea(
+                                    attrs={'placeholder': 'Poinformuj swoich partnerów o wymarzonym pomyśle wakacyjnym. '
                                                           'Dokończ powyższe zdanie i wygenerwuj plik do wstawienia '
                                                           'w stopkę maila.'}))
