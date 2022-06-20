a3e28827c453e953560040e9f8e97d81541e783f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-20 12:42:25 +0200

    Configure template.

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {'phrase_to_render': "popo"}
+    return render(request, "generator/index.html", context)
