3a6241b6b23c59ea063f15a5de8850755300a0a9
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-21 23:22:32 +0200

    Update image template.

--- generator/forms.py
+++ generator/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class IdeaForm(forms.Form):
     user_idea = forms.CharField(label='Mamy taki pomysł, żeby...',
-                                max_length=50,
+                                max_length=100,
                                 widget=forms.Textarea(
                                     attrs={'placeholder': 'Poinformuj swoich partnerów o wymarzonym pomyśle wakacyjnym. '
                                                           'Dokończ powyższe zdanie i wygenerwuj plik do wstawienia '
