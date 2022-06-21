ce8c7a5a6af7e8b6272d11fb5dda0151860a7c58
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-22 00:21:16 +0200

    Add --default-background-color=0 flag.

--- generator/views.py
+++ generator/views.py
@@ -18,7 +18,7 @@ def download(request):
     phrase = {'phrase': user_idea}
     content = render_to_string('generator/image.html', phrase)
     hti = Html2Image()
-    hti.browser.flags = ['--force-color-profile=SRGB', '--disable-gpu', '--no-sandbox']
+    hti.browser.flags = ['--force-color-profile=SRGB', '--disable-gpu', '--no-sandbox', '--default-background-color=0']
     hti.screenshot(html_str=content, size=(600, 200), save_as='generated-footer.png')
     img = open('./generated-footer.png', 'rb')
     response = FileResponse(img)
