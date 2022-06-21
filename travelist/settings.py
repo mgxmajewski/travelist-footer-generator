f166c50d4ee7fa6716c375f0ff7dabf426653570
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-21 02:16:33 +0200

    Change static resources.

--- travelist/settings.py
+++ travelist/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = '/home/travelist/travelist-footer-generator/generator/static/'
+STATIC_ROOT = '/home/travelist/travelist-footer-generator/generator/static/generator/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
