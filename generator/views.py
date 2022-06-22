ae81aa2f1897ba993c5754b99066c929a8770cd8
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-22 18:22:02 +0200

    Update content.

--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
-from django.http import FileResponse
 from django.views.decorators.csrf import csrf_exempt
 from html2image import Html2Image
 
@@ -13,7 +12,6 @@ def index(request):
     if not request.session.session_key:
         request.session.create()
 
-    # print(f'session {request.session.session_key}')
     return render(request, "generator/index.html", context)
 
 
@@ -21,14 +19,11 @@ def index(request):
 def download(request):
     user_idea = request.POST.get('user_idea')
     print(user_idea)
-    # print(request.session.session_key)
     print(f'session {request.session.session_key}')
     phrase = {'phrase': user_idea}
     content = render_to_string('generator/image.html', phrase)
     hti = Html2Image()
     hti.browser.flags = ['--force-color-profile=SRGB', '--disable-gpu', '--no-sandbox', '--default-background-color=0']
     hti.output_path = './generator/static/generator'
-    hti.screenshot(html_str=content, size=(600, 200), save_as='generated-footer.png')
-    # img = open('./generator/static/generator/generated-footer.png', 'rb')
-    # response = FileResponse(img)
-    return redirect('/static/generator/generated-footer.png')
+    hti.screenshot(html_str=content, size=(600, 200), save_as=f'generated-footer-{request.session.session_key[:5]}.png')
+    return redirect(f'/static/generator/generated-footer-{request.session.session_key[:5]}.png')
