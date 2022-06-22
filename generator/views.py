15d39b64f817b56f556b2102944224530a19c79e
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-22 17:46:45 +0200

    Adjust rendering mechanism.

--- generator/views.py
+++ generator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from django.http import FileResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -10,6 +10,10 @@ from .forms import IdeaForm
 def index(request):
     form = IdeaForm()
     context = {'form': form}
+    if not request.session.session_key:
+        request.session.create()
+
+    # print(f'session {request.session.session_key}')
     return render(request, "generator/index.html", context)
 
 
@@ -17,11 +21,14 @@ def index(request):
 def download(request):
     user_idea = request.POST.get('user_idea')
     print(user_idea)
+    # print(request.session.session_key)
+    print(f'session {request.session.session_key}')
     phrase = {'phrase': user_idea}
     content = render_to_string('generator/image.html', phrase)
     hti = Html2Image()
     hti.browser.flags = ['--force-color-profile=SRGB', '--disable-gpu', '--no-sandbox', '--default-background-color=0']
+    hti.output_path = './generator/static/generator'
     hti.screenshot(html_str=content, size=(600, 200), save_as='generated-footer.png')
-    img = open('./generated-footer.png', 'rb')
-    response = FileResponse(img)
-    return response
+    # img = open('./generator/static/generator/generated-footer.png', 'rb')
+    # response = FileResponse(img)
+    return redirect('/static/generator/generated-footer.png')
